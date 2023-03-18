a39f14622994962a1be8a716482f70cfa3305a92
kevin Wairi <kevinwairi@gmail.com>
2023-03-18 23:56:10 +0300

    Assignment completed

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+function saturdayFun(activity="roller-skate"){
+    return `This Saturday, I want to ${activity}!`;
+}
+saturdayFun();
+
+const mondayWork=function (activity1="go to the office"){
+    return `This Monday, I will ${activity1}.`;
+}
+mondayWork();
+// a fullstop almost made me sweat off this test 
+
+function wrapAdjective(aste='*'){
+    return function(str='special'){
+        return `You are ${aste}${str}${aste}!`;
+    }
+}
+wrapAdjective()();
+console.log(wrapAdjective()());
